b82ed9159ab5be265207328d3109334d20b614da
xxin0103 <71486069+xxin0103@users.noreply.github.com>
2020-12-18 17:49:31 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://xxin0103.github.io/aifrog/model.json';
 
 
 function preload() {
